45843dccfc9b6f46f5ea9091187bc7040c7064ae
priyadarshanipanigrahy28 <117714258+priyadarshanipanigrahy28@users.noreply.github.com>
2023-10-15 19:32:00 +0530

    Add files via upload

---
+++ lower-upper.c
@@ -0,0 +1,10 @@
+//Convert lower case to upper case
+
+#include<stdio.h>
+int main()
+{
+	char ch;
+	printf("Enter a lower case character:");
+	scanf("%c",&ch);
+	printf("Upper case character is:%c",ch-32);
+}
